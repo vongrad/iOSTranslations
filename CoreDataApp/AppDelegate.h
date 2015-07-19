10fd8488ee3cacbcce27cdb91400cb5ee05ed2bd
Adam Vongrej <adamv@Adams-MacBook-Air.local>
2015-07-19 14:13:50 +0200

    Initial Commit

---
+++ CoreDataApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CoreDataApp
+//
+//  Created by Adam Vongrej on 19/07/15.
+//  Copyright (c) 2015 Adam Vongrej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CoreDataApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataApp
+//
+//  Created by Adam Vongrej on 19/07/15.
+//  Copyright (c) 2015 Adam Vongrej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
